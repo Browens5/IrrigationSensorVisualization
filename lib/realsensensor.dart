c0559ad13b341f6f45f13b743cc5d54e17337bfd
Browens5 <64341767+Browens5@users.noreply.github.com>
2023-10-31 12:41:04 -0600

    Update realsensensor.dart

--- lib/realsensensor.dart
+++ lib/realsensensor.dart
@@ -13,7 +13,7 @@ class RealSensor implements SensorManager {
     var s = Serial('/dev/ttyS0', Baudrate.b9600);
 
     try {
-      s.writeString("2\r\n");
+      // s.writeString("2");
 
       var event = s.read(256, 1000);
       String humid = event.toString();
