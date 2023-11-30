ed74d4139c3465a3b5ce77a2eee263f4e70cdb14
Browens5 <64341767+Browens5@users.noreply.github.com>
2023-11-30 09:25:26 -0700

    Update chart.dart

--- lib/chart.dart
+++ lib/chart.dart
@@ -17,23 +17,59 @@ class ChartWidget extends StatelessWidget {
   final String axisName;
   final Color color;
   final bool showTitle;
+  final double fsize = 18;
 
   @override
   Widget build(BuildContext context) {
     return SfCartesianChart(
+      borderColor: const Color.fromARGB(0, 0, 0, 0),
       // Initialize category axis
       title: ChartTitle(
-          text: showTitle ? title : "",
-          borderWidth: 2,
-          // Aligns the chart title to left
-          alignment: ChartAlignment.center,
-          textStyle: const TextStyle(
-            color: Color.fromARGB(255, 5, 0, 101),
-          )),
-      primaryXAxis: NumericAxis(title: AxisTitle(text: 'Time (a.u.)')),
+        text: showTitle ? title : "",
+        borderWidth: 2,
+        // Aligns the chart title to left
+        alignment: ChartAlignment.center,
+        textStyle: TextStyle(
+            color: const Color.fromARGB(255, 0, 0, 0),
+            fontSize: fsize - 2,
+            fontFamily: 'Serif'),
+      ),
+      primaryXAxis: NumericAxis(
+        title: AxisTitle(
+          text: 'Time (a.u.)',
+          textStyle: TextStyle(
+            fontSize: fsize,
+            fontFamily: 'Serif',
+          ),
+        ),
+        labelStyle: TextStyle(
+          fontSize: fsize,
+          fontFamily: 'Serif',
+          color: const Color.fromARGB(255, 0, 0, 0),
+        ),
+        majorGridLines: const MajorGridLines(
+          width: 0,
+        ),
+      ),
       primaryYAxis: NumericAxis(
-          title: AxisTitle(text: axisName),
-          labelAlignment: LabelAlignment.center),
+        title: AxisTitle(
+          text: axisName,
+          textStyle: TextStyle(
+            fontSize: fsize,
+            fontFamily: 'Serif',
+          ),
+        ),
+        labelAlignment: LabelAlignment.center,
+        labelStyle: TextStyle(
+          fontSize: fsize,
+          fontFamily: 'Serif',
+          color: const Color.fromARGB(255, 0, 0, 0),
+        ),
+        majorGridLines: const MajorGridLines(
+          width: 0,
+        ),
+      ),
+
       series: <ChartSeries>[
         // Initialize line series
 
